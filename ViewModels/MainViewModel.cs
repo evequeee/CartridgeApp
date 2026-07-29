96689bf50525ca0ab791d5ff3bacfad10eb4b680
evequeee <bogdankors1@gmail.com>
2026-07-29 22:35:38 +0300

    Add SpinCommand placeholder for revolver roulette

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using CartridgeApp.Models;
 using CartridgeApp.Services;
 
@@ -7,13 +8,20 @@ namespace CartridgeApp.ViewModels;
 public class MainViewModel
 {
     private readonly MockGameService _gameService;
-
     public ObservableCollection<Game> Games { get; set; }
+
+    public ICommand SpinCommand { get; private set; }
 
     public MainViewModel()
     {
         _gameService = new MockGameService();
-
         Games = new ObservableCollection<Game>(_gameService.GetGames());
+
+        SpinCommand = new Command(SpinRevolver);
+    }
+
+    private void SpinRevolver()
+    {
+        // TODO
     }
 }

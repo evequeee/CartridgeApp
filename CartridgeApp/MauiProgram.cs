e26444d3e626c91611ef34dff255a2dcc72a5114
evequeee <bogdankors1@gmail.com>
2026-07-30 22:37:56 +0300

    Register MockGameService and MainViewModel in DI container

--- CartridgeApp/MauiProgram.cs
+++ CartridgeApp/MauiProgram.cs
@@ -1,24 +1,30 @@
 ﻿using Microsoft.Extensions.Logging;
 
+using CartridgeApp.Services;
+using CartridgeApp.ViewModels;
+
 namespace CartridgeApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        builder.Services.AddSingleton<MockGameService>();
+        builder.Services.AddTransient<MainViewModel>();
+
+        return builder.Build();
+    }
 }
